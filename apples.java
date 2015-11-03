2c8a2188522767e44d52583403b647f2b1dddee4
Cameron Kirk <kirkster96@gmail.com>
2015-11-02 17:17:41 -0800

    apples.java
    This is an example code I used to learn and understand what methods are in Java. There is a method called "main" that is the first thing that the computer looks at when it tries to execute a script.

---
+++ apples.java
@@ -0,0 +1,7 @@
+public class apples {
+	public static void main (String args[]){
+		System.out.println("Hey look this text comes from a command in");
+		 System.out.println("the method body from the main method");
+		
+	}
+}
