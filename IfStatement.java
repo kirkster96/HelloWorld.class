5f7475cba6da69e97b9540e6177a996b1dc23627
Cameron Kirk <kirkster96@gmail.com>
2015-11-04 00:37:29 -0800

    If/Else statements & Testing Operators
    In this code I learned how to use the if/else statements with in the main function and learned about the basic testing operators. I can see that this has a lot to do with Boolean logic which is something I have already learned about in the past.

---
+++ IfStatement.java
@@ -0,0 +1,18 @@
+public class IfStatment {
+	public static void main(String args[]){
+		int x = 6;
+		/*different operators used for testing are
+		 * <,>,==,!=,<=,>=
+		 */
+		if (x == 9){
+			System.out.println("yes");
+			/*This is the statement body and is executed
+			 *  if condition is true
+			 */
+		}else{
+			System.out.println("this is else");
+			
+		}
+		
+	}
+}
